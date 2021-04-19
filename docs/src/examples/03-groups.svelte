6ed2770eea0bbfbf175fdfef4d771ab89c471cda
Martin Skočík <mskocik@gmail.com>
2021-04-19 12:48:09 +0200

    move docs to separate repo

--- docs/src/examples/03-groups.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import Svelecte from '../../../src/svelecte.js';
-  import { dataset } from '../data.js';
-</script>
-
-<Svelecte options={dataset.countryGroups()}></Svelecte>
--- docs/src/examples/06-fetch.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import Svelecte from '../../../src/svelecte.js';
-</script>
-
-<Svelecte
-  placeholder="Start typing ('re' for example)"
-  fetch="https://my-json-server.typicode.com/mskocik/svelecte-db/colors?value_like=[query]"
-></Svelecte>
