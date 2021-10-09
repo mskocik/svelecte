522860385364b5ad6eb7c4617e5fd54b3c4dfd3f
Martin Skočík <mskocik@gmail.com>
2021-10-09 12:53:00 +0200

    docs update

--- docs/src/03-groups.svelte
+++ docs/src/03-groups.svelte
@@ -1,6 +1,65 @@
 <script>
   import Svelecte from '../../src/Svelecte.svelte';
-  import { dataset } from './data.js';
+
+  const options = [
+    {
+      label: 'A',
+      options: [{
+        value: 'al',
+        text: 'Albania'
+      },
+      {
+        value: 'ad',
+        text: 'Andorra'
+      },
+      {
+        value: 'am',
+        text: 'Armenia'
+      },
+      {
+        value: 'a',
+        text: 'Austria'
+      },
+      {
+        value: 'az',
+        text: 'Azerbaijan'
+      }]
+    },
+    {
+      label: 'B',
+      options: [{
+        value: 'by',
+        text: 'Belarus'
+      },
+      {
+        value: 'be',
+        text: 'Belgium'
+      },
+      {
+        value: 'ba',
+        text: 'Bosnia and Herzegovina'
+      },
+      {
+        value: 'bg',
+        text: 'Bulgaria'
+      }]
+    },
+    {
+      label: 'C',
+      options: [{
+        value: 'hr',
+        text: 'Croatia'
+      },
+      {
+        value: 'cy',
+        text: 'Cyprus'
+      },
+      {
+        value: 'cz',
+        text: 'Czechia'
+      }]
+    }
+  ]
 </script>
 
-<Svelecte options={dataset.countryGroups()}></Svelecte>
+<Svelecte {options}></Svelecte>
