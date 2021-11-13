229f3608b724cf5d5154125070ba8012d204b8b8
Martin Skočík <mskocik@gmail.com>
2021-11-13 09:58:25 +0100

    replace <a> by <button> element

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -27,9 +27,9 @@
 >
   {@html highlightSearch(item, isSelected, inputValue, formatter, disableHighlight)}
 {#if isSelected && isMultiple}
-  <a href="#deselect" class="sv-item-btn" tabindex="-1" data-action="deselect">
+  <button class="sv-item-btn" tabindex="-1" data-action="deselect">
     <svg height="16" width="16" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg>
-  </a>
+  </button>
 {/if}
 </div>
 {/if}
@@ -80,6 +80,10 @@
   padding-right: 4px;
   box-sizing: border-box;
   border-radius: 2px;
+  border-width: 0;
+  margin: 0;
+  cursor: pointer;
+
 }
 :global(.sv-item-btn:hover) { background-color: #ddd; }
 :global(.highlight) { background-color: yellow; }
