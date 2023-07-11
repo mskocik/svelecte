f17a5829a1170dc2bba1b6b8f05e64f82e7ee4f7
Martin Skočík <mskocik@gmail.com>
2023-07-11 15:00:46 +0200

    fix error when exporting custom-element

--- src/lib/component.js
+++ src/lib/component.js
@@ -1,4 +1,6 @@
-import Svelecte, { addFormatter, config, registerSvelecte } from './index.js';
+import Svelecte, { addFormatter, config } from './index.js';
+
+import { registerSvelecte } from './lib/custom-element.js';
 
 export { addFormatter, config };
 export const registerAsCustomElement = (name) => registerSvelecte(name || 'el-svelecte', Svelecte, config);
--- src/lib/index.js
+++ src/lib/index.js
@@ -2,4 +2,3 @@ import Svelecte from './Svelecte.svelte';
 
 export default Svelecte;
 export { addFormatter, config, TAB_SELECT_NAVIGATE } from './Svelecte.svelte';
-export { registerSvelecte } from './lib/custom-element.js';
