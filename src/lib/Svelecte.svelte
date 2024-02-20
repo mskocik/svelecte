2690b079ed70756ab28378dad97a4be1114816e2
Martin Skočík <mskocik@gmail.com>
2024-02-20 16:29:17 +0100

    rename dropdown toggle slot [BC break]

--- src/lib/Svelecte.svelte
+++ src/lib/Svelecte.svelte
@@ -1155,7 +1155,6 @@
       on:finalize={onDndEvent}
     >
       {#if selectedOptions.length }
-      <!-- TODO: re-implement comments -->
       {#if multiple && doCollapse}
         <slot name="collapsedSelection" {selectedOptions} i18n={i18n_actual}>{i18n_actual.collapsedSelection(selectedOptions.length)}</slot>
       {:else}
@@ -1224,7 +1223,7 @@
         data-action="toggle" tabindex="-1"
       >
         <!-- TODO: rename this slot -->
-        <slot name="indicator-icon" hasDropdownOpened={is_dropdown_opened}>
+        <slot name="dropdown-toggle" hasDropdownOpened={is_dropdown_opened}>
           <svg class="indicator-icon" viewBox="0 0 20 20" aria-hidden="true" focusable="false">
             <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
           </svg>
