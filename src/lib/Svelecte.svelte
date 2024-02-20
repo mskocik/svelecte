873e62f42b358e2c3db44d45db996bf855813a2b
Martin Skočík <mskocik@gmail.com>
2024-02-20 16:34:03 +0100

    add 'create-row' slot and change how i18n.createRowLabel is rendered [BC break]

--- src/lib/Svelecte.svelte
+++ src/lib/Svelecte.svelte
@@ -1222,7 +1222,6 @@
       <button type="button" class="sv-btn-indicator" class:sv-dropdown-opened={is_dropdown_opened}
         data-action="toggle" tabindex="-1"
       >
-        <!-- TODO: rename this slot -->
         <slot name="dropdown-toggle" hasDropdownOpened={is_dropdown_opened}>
           <svg class="indicator-icon" viewBox="0 0 20 20" aria-hidden="true" focusable="false">
             <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
@@ -1304,11 +1303,10 @@
           class:active={(options_filtered.length ? options_filtered.length : 0) === dropdown_index}
           class:is-disabled={alreadyCreated.includes(input_value)}
         >
-          <!-- TODO: make it a slot -->
-          <span class:is-loading={isCreating}>
-            {@html i18n_actual.createRowLabel(input_value)}
-          </span>
-          <span class="shortcut"><kbd>{meta_key}</kbd>+<kbd>Enter</kbd></span>
+          <slot name="create-row" {isCreating} inputValue={input_value} i18n={i18n_actual}>
+            <span class:is-loading={isCreating}>{i18n_actual.createRowLabel(input_value)}</span>
+            <span class="shortcut"><kbd>{meta_key}</kbd>+<kbd>Enter</kbd></span>
+          </slot>
         </button>
       </div>
     {/if}
