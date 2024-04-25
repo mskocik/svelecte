3322d76f85275539e39100eedb85ea7e03d1a97f
Martin Skočík <mskocik@gmail.com>
2024-04-25 06:39:19 +0200

    extend custom-element related docs

--- src/routes/examples/+page.svelte
+++ src/routes/examples/+page.svelte
@@ -181,10 +181,8 @@ Reorder selection by dragging: {value}
 
 ## Custom element
 
-Svelecte can be used outside svelte projects. I have used it successfully in Vue and PHP projects myself.
-
-<select id="original"></select>
-<el-svelecte options={options} placeholder="Pick a color" />
+Svelecte can be used outside svelte projects. Almost every commonly used property _should_ be available. Also
+dependend selects are possible with custom elements. I have used it successfully in Vue and PHP projects myself.
 
 Check the source and look for `<el-svelecte />` element.
 
@@ -203,6 +201,16 @@ Most of properties is supported with one change: `parentValue`.
 You define `parent` attribute instead. It represents html `id` attribute of parent select.
 This attribute should be defined on child svelecte element.
 
+Native `<select>` element can be used as "anchor element", which will serve as underlying element for Svelecte component.
+Svelecte component can inherit `required`, `multiple` and `disabled` properties from `<select>` element and in case `options`
+property is not set, it can extract option list from `<option>` elements.
+
+```html
+  <select id="my_select" name="form_select" required>
+    <option>...</option>
+  </select>
+  <el-svelecte placeholder="Pick an item"/>
+```
 <style>
   :global(.inlined) {
     display: inline-flex;
